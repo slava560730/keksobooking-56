56f765cde178bc3111701293f02c833cc69f524e
GUNMAXbit <gunmax9696@gmail.com>
2023-07-12 23:20:28 +0500

    Add Object AUTHOR(complete)

--- js/main.js
+++ js/main.js
@@ -13,30 +13,45 @@ function getRandomNumber (min, max) {
   const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
   const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
   const result = Math.floor(Math.random() * (upper - lower + 1) + lower);
-  if (result < 10) {
-    return `0${  result}`;
-  }
   return result;
 }
+let numbers = [];
 
-function getRandomUnicNumber (getRandomNumber) {
-  return getRandomNumber;
+function createNumbers (min, max) {
+  for (let i = min; i <= max; i++) {
+    numbers.push(i);
+}
+  return numbers;
 }
 
 
+function createImgNumber () {
+  // const newImages = [];
+  const randomIndex = getRandomNumber(0, createNumbers (1, 10).length - 1);
+  const random = Number(createNumbers (1, 10).splice(randomIndex, 1));
+  // newImages.push(random);
+  return random < 10 ? `0${random}`: random;
+}
 
-const AUTHOR = {
-  avatar: 'img/avatars/user' + getRandomUnicNumber(getRandomNumber(0, 10)) + '.png,'
-};
+// const OFFER = {
+//   title: 'random',
+//   address: '{location.lat}, {{location.lng}',
+//   price: getRandomNumber(1000, 10000)
+// };
 
 const createAdvertisements = () => {
+  const author = {
+    avatar: 'img/avatars/user' + createImgNumber () + '.png,'
+  };
+
   return {
-    AUTHOR
+    author
   };
 };
 
-const similarAdvertisements = Array.from({length: 3}, createAdvertisements);
+const similarAdvertisements = Array.from({length: 10}, createAdvertisements);
 
+// eslint-disable-next-line no-console
 console.log(similarAdvertisements);
 
 
