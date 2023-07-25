d3d6c50a48a92cbf9e142acd7a661ef4eb6b2393
GUNMAXbit <gunmax9696@gmail.com>
2023-07-25 06:58:01 +0500

    module8-task1 start

--- js/form.js
+++ js/form.js
@@ -3,6 +3,25 @@ const formFieldsets = formContainer.querySelectorAll('fieldset');
 const filtersContainer = document.querySelector('.map__filters');
 const filtersFieldsets = filtersContainer.querySelectorAll('fieldset');
 
+const pristine = new Pristine(formContainer);
+
+// function validateTitle (value) {
+//   return value.length >= 30 && value.length <= 100;
+// }
+
+pristine.addValidator(formContainer.querySelector('#title'), validateTitle, 'От 30 до 100 символов');
+
+formContainer.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValide = pristine.validate();
+
+  if(isValide){
+    console.log('Форма валидна!');
+  } else {
+    console.log('Форма не валидна!');
+  }
+});
+
 function disableForm() {
   formContainer.classList.add('ad-form--disabled');
   filtersContainer.classList.add('map__filters--disabled');
