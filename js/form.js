9238f424bf85b003619175a54dab3a7f85b1c781
unknown <gunmax9696@gmail.com>
2023-07-25 09:09:09 +0500

    module8-task1, create validate functions

--- js/form.js
+++ js/form.js
@@ -2,22 +2,41 @@ const formContainer = document.querySelector('.ad-form');
 const formFieldsets = formContainer.querySelectorAll('fieldset');
 const filtersContainer = document.querySelector('.map__filters');
 const filtersFieldsets = filtersContainer.querySelectorAll('fieldset');
+const error = document.querySelector('ad-form__error-text');
 
-const pristine = new Pristine(formContainer);
+const pristine = new Pristine(formContainer, {
+  classTo: 'ad-form__element',
+  successClass: 'ad-form__element--valid',
+  errorClass: 'ad-form__element--invalid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'ad-form__error-text',
+});
 
-// function validateTitle (value) {
-//   return value.length >= 30 && value.length <= 100;
-// }
+function validateTitle (value) {
+  return value.length >= 30 && value.length <= 100;
+}
+
+function validatePrice (value) {
+  return value <= 100000;
+}
+
+function validateRooms (value) {
+  return value
+}
+
+
 
 pristine.addValidator(formContainer.querySelector('#title'), validateTitle, 'От 30 до 100 символов');
+pristine.addValidator(formContainer.querySelector('#price'), validatePrice, 'Максимальное значение — 100 000');
 
 formContainer.addEventListener('submit', (evt) => {
-  evt.preventDefault();
   const isValide = pristine.validate();
 
   if(isValide){
     console.log('Форма валидна!');
   } else {
+    evt.preventDefault();
     console.log('Форма не валидна!');
   }
 });
